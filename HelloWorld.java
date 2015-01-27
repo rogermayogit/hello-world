59f990dba05471e243006b27e403154cf2141ffc
rogermayogit <mayoatech@gmail.com>
2015-01-27 07:56:33 -0800

    Added HelloWorld.java
    Added HelloWorld.java with code and credit given to Ryan H. for helping
    add code to the project.

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+//Thank you Ryan for helping me add files to a project!
+
+public class HelloWorld
+{
+    public static void main(String []args)
+    {
+        System.out.println("Hello World")
+    }
+
+}
